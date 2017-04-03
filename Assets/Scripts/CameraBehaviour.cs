a466ee9e09d5d5930ce4029ebd692843d3d5f945
BLUE-BLUR\Lerenwe <johan.nagy.contact@gmail.com>
2017-04-03 19:11:07 +0200

    Smoothen out Pauline Moves + Better horizontal alignment for Camera

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -6,25 +6,52 @@ public class CameraBehaviour : MonoBehaviour {
 
     public Transform Target;
     public float distanceFromTarget;
+    public float lateralDistanceFromTarget = 3f;
+    public float GlobalDamp = .1f;
+    public float MarginDamp = 5f;
+
+    bool goingLeft = false;
+
+    float margin = 0f;
 
     Vector3 targetPosition;
 
 	// Use this for initialization
 	void Start ()
     {
-        MoveToTargetPoint();
+        MoveToTargetPoint(false);
 	}
 
-    void MoveToTargetPoint()
+    void MoveToTargetPoint(bool Smooth)
     {
+        if (Input.GetAxisRaw("Horizontal") > 0)
+            goingLeft = true;
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+            goingLeft = false;
+
+        if (goingLeft)
+            lateralDistanceFromTarget = Mathf.Lerp(lateralDistanceFromTarget, 3, MarginDamp);
+        else
+            lateralDistanceFromTarget = Mathf.Lerp(lateralDistanceFromTarget, -3, MarginDamp);
+
         targetPosition = Target.position;
+        targetPosition.x += lateralDistanceFromTarget;
+        targetPosition.y += 1f;
         targetPosition.z -= distanceFromTarget;
-        transform.position = targetPosition;
+
+        if (Smooth)
+        {
+            transform.position = Vector3.Slerp(transform.position, targetPosition, GlobalDamp);   
+        }
+        else
+        {
+            transform.position = targetPosition;
+        }
     }
 
 	// Update is called once per frame
 	void LateUpdate ()
     {
-        MoveToTargetPoint();
+        MoveToTargetPoint(true);
     }
 }
--- Assets/Scripts/Characters/Player.cs
+++ Assets/Scripts/Characters/Player.cs
@@ -7,7 +7,7 @@ public class Player : Characters {
     bool jump = false;
 
 	// Update is called once per frame
-	void FixedUpdate ()
+	void Update ()
     {
         jump = Input.GetButtonDown("Jump");
         Move(Input.GetAxisRaw("Horizontal"), jump);
