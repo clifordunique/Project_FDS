509e957979e99a7e5d8a9ef80e694250492323c4
BLUE-BLUR\Lerenwe <johan.nagy.contact@gmail.com>
2017-04-19 18:42:20 +0200

    Stairs can be now climbed on the right side too, MUCH ROCKET SCIENCE

--- Assets/Scripts/Characters/Characters.cs
+++ Assets/Scripts/Characters/Characters.cs
@@ -39,78 +39,125 @@ public class Characters : MonoBehaviour {
     CharactersSharedVariables sharedVariables;
     #endregion
 
-    #region experimental
+    #region Stairs Detection
     public float stepMaxHeight;
     public float stepLengthDetection = 1f;
     float closeToStepPercent = 0f;
     bool OnStep = false;
-    float groundPointForStep = 0f;
+    float currentStepHeight = 0f;
     List<Collider> ignoredColliders = new List<Collider>();
     #endregion
 
-    void CheckStep ()
+    void VerticalCheckStep (RaycastHit horizontalHit, bool LeftCheck)
     {
-        RaycastHit horizontalHit;
-        RaycastHit verticalHit;
-        RaycastHit secondVerticalHit;
-        Vector3 horizontalOrigin = new Vector3(thisCollider.bounds.min.x + .01f, thisCollider.bounds.min.y + .01f, transform.position.z);
+        RaycastHit verticalHit; //This ray will be cast against the upper step
+        RaycastHit secondVerticalHit; //While this one will be cast against the lower step or ground to calculate the step's height
 
-        if (Physics.Raycast(horizontalOrigin, -transform.right, out horizontalHit, stepLengthDetection))
+        //Seems so! Now, we prepare another Raycast, whose origin is set up to be above the hit point of the previous cast
+        //This is to make sure it's a small step we hit, and not a mid-air platform or a wall
+        Vector3 verticalOrigin; 
+        
+        if(LeftCheck)
+            verticalOrigin = new Vector3(horizontalHit.point.x - .01f, thisCollider.bounds.max.y - .05f, transform.position.z);
+        else
+            verticalOrigin = new Vector3(horizontalHit.point.x + .01f, thisCollider.bounds.max.y - .05f, transform.position.z);
+
+        if (Physics.Raycast(verticalOrigin, -transform.up, out verticalHit, thisCollider.bounds.size.y))
         {
-            //Debug.Log("Hit = " + horizontalHit.point);
-            //Debug.DrawLine(horizontalOrigin, horizontalHit.point, Color.red);
-            Vector3 verticalOrigin = new Vector3(horizontalHit.point.x - .01f, thisCollider.bounds.max.y - .05f, transform.position.z);
+            //Okay, we hit something, but it can still be the ground... We'll have to measure the distance between what we just hit and what's on the other
+            //side of the step (or whatever we just hit). If the distance is greater than the step max height we set up, then... We don't consider this as a step, and just give up.
 
-            if (Physics.Raycast(verticalOrigin, -transform.up, out verticalHit, thisCollider.bounds.size.y))
-            {
+            if(LeftCheck)
                 Physics.Raycast(verticalOrigin + transform.right * .1f, -transform.up, out secondVerticalHit, Mathf.Infinity);
-                float distanceBetweenStepAndGround = verticalHit.point.y - secondVerticalHit.point.y;
+            else
+                Physics.Raycast(verticalOrigin - transform.right * .1f, -transform.up, out secondVerticalHit, Mathf.Infinity);
 
-                Debug.DrawLine(verticalOrigin, verticalOrigin - transform.up * thisCollider.bounds.size.y);
+            float distanceBetweenStepAndGround = verticalHit.point.y - secondVerticalHit.point.y;
 
-                if (distanceBetweenStepAndGround <= stepMaxHeight)
-                {
-                    //Debug.DrawLine(verticalOrigin, verticalHit.point, Color.red);
-                    Vector3 stepEdge = new Vector3(horizontalHit.point.x, verticalHit.point.y, transform.position.z);
-                    Debug.DrawLine(transform.position, stepEdge, Color.red);
+            Debug.DrawLine(verticalOrigin, verticalOrigin - transform.up * thisCollider.bounds.size.y);
+
+            //So if the distance we just measured is below the stepMaxHeight we set up...
+            if (distanceBetweenStepAndGround <= stepMaxHeight)
+            {
+                //Then congratulations, it's a step Pauline can climb! \o/
+
+                //The next to line are just used for debugging purpose...
+                Vector3 stepEdge = new Vector3(horizontalHit.point.x, verticalHit.point.y, transform.position.z);
+                Debug.DrawLine(transform.position, stepEdge, Color.red);
+
+                //All right, now we have to calculate the percentage of proximity between Pauline and the step, so we can calculate how high she'll need to be.
+                if(LeftCheck)
                     closeToStepPercent = Mathf.Abs(thisCollider.bounds.min.x - horizontalHit.point.x) / stepLengthDetection;
-                    closeToStepPercent = Mathf.Abs(1 - closeToStepPercent);
-
-
-                    Physics.IgnoreCollision(verticalHit.collider, thisCollider, true);
-                    ignoredColliders.Add(verticalHit.collider);
-
-
-                    Debug.DrawLine(transform.position, secondVerticalHit.point, Color.blue);
-
-                    groundPointForStep = Mathf.Abs(distanceBetweenStepAndGround);
-
-                    Debug.Log("Step ahead. Proximity percent = " + closeToStepPercent + ". Step height = " + groundPointForStep);
-
-                    //Physics.Raycast (verticalOrigin);
-
-                    float targetHeight = Mathf.Lerp(secondVerticalHit.point.y + thisCollider.bounds.size.y / 2, verticalHit.point.y + groundPointForStep + thisCollider.bounds.size.y / 2, closeToStepPercent);
-                    transform.position = new Vector3(transform.position.x, targetHeight, transform.position.z);
-                    Debug.DrawLine(transform.position, new Vector3(transform.position.x, targetHeight, transform.position.z), Color.red);
-                    OnStep = true;
-                }
                 else
-                    OnStep = false;
+                    closeToStepPercent = Mathf.Abs(thisCollider.bounds.max.x - horizontalHit.point.x) / stepLengthDetection;
+
+                closeToStepPercent = Mathf.Abs(1 - closeToStepPercent);
+
+                //Btw, we should deactivate any physic calculation between the stairs and Pauline, or else, it's going to be buggy as hell...
+                Physics.IgnoreCollision(verticalHit.collider, thisCollider, true);
+                ignoredColliders.Add(verticalHit.collider); //this list will come in handy later, when Pauline gets off or gets on top of the stairs to make
+                                                            //sure the physic engine will get to work on those stairs again. 
+
+
+                Debug.DrawLine(transform.position, secondVerticalHit.point, Color.blue);
+
+                //Make sure the height we calculated before is a positive number, it'll come handy later.
+                currentStepHeight = Mathf.Abs(distanceBetweenStepAndGround);
+                Debug.Log("Step ahead. Proximity percent = " + closeToStepPercent + ". Step height = " + currentStepHeight);
+
+                //Then, we use all those previous datas to calculate the new height for Pauline.
+                //To summarize, the closer Pauline will be to the step, the higher she'll need to be, so that it's like she's walking up a ramp.
+                float targetHeight = Mathf.Lerp(secondVerticalHit.point.y + thisCollider.bounds.size.y / 2, verticalHit.point.y + currentStepHeight + thisCollider.bounds.size.y / 2, closeToStepPercent);
+
+                //Then let's apply all this directly to the transform component
+                transform.position = new Vector3(transform.position.x, targetHeight, transform.position.z);
+                Debug.DrawLine(transform.position, new Vector3(transform.position.x, targetHeight, transform.position.z), Color.red);
+
+                //And flag the global bool OnStep to true, so that we can apply some exceptions to Pauline's moves
+                //(Like overriding the fact she's grounded even if technically, her collider can't detect anything below her feet as we deactivated the
+                //collisions between her collider and the stairs, remember?)
+                OnStep = true;
             }
             else
-            {
                 OnStep = false;
-            }
         }
         else
+        {
             OnStep = false;
+        }
+    }
 
-        if (!OnStep)
+    void CheckStep () //Checks if we're facing a step from a staircase that Pauline can climb easily
+    {
+        RaycastHit horizontalHit;
+
+        //Setting up the first Raycast Origin : Roughly starts from Pauline's lowest collider boundary
+        Vector3 horizontalLeftOrigin = new Vector3(thisCollider.bounds.min.x + .01f, thisCollider.bounds.min.y + .01f, transform.position.z);
+
+        //There we go, let's check if there's something in front of Pauline's feet (left side)
+        if (Physics.Raycast(horizontalLeftOrigin, -transform.right, out horizontalHit, stepLengthDetection))
+        {
+            VerticalCheckStep(horizontalHit, true);
+        }
+        else //If there's nothing on her left side, let's check on the right side
+        {
+            Vector3 horizontalRightOrigin = new Vector3(thisCollider.bounds.max.x - .01f, thisCollider.bounds.min.y + .01f, transform.position.z);
+
+            if (Physics.Raycast(horizontalRightOrigin, transform.right, out horizontalHit, stepLengthDetection))
+            {
+                VerticalCheckStep(horizontalHit, false);
+            }
+            else
+                OnStep = false;
+        }
+
+        if (!OnStep) //If we aren't on a step, or on top of the stairs, we should reactivate the collisions between them and Pauline.
         {
             foreach (Collider ignoredCollider in ignoredColliders)
             {
                 Physics.IgnoreCollision(thisCollider, ignoredCollider, false);
             }
+
             ignoredColliders.Clear();
         }
     }
